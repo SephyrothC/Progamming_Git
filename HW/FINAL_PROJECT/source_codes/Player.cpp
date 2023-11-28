1953e83b3371928b33d8fd984c6de6e4254b4bd6
SephyrothC <as.grifon@gmail.com>
2023-11-28 16:24:35 +0900

    Final project init

---
+++ HW/FINAL_PROJECT/source_codes/Player.cpp
@@ -0,0 +1,115 @@
+#include "Player.h"
+#include "Game.h"
+
+std::default_random_engine Player::random_engine(time(nullptr));
+std::uniform_int_distribution<unsigned int> Player::ability_range(0, 100);
+std::uniform_real_distribution<float> Player::possibility(0.f, 1.f);
+
+float PlayerRLGL::fallDownRate = 0.1f;
+
+Player::Player(int number)
+{
+	this->number = number;
+
+	agility = ability_range(random_engine);
+	fearlessness = ability_range(random_engine);
+}
+
+Player::Player(int number, int agility, int fearlessness)
+{
+	this->number = number;
+	this->agility = agility;
+	this->fearlessness = fearlessness;
+}
+
+
+
+
+
+bool PlayerRLGL::act()
+{
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+void PlayerRLGL::dyingMessage()
+{ 
+	if (isPlaying())
+	{
+		printStatus();
+		std::cout << " is still on the ground and died." << std::endl;
+	}
+		
+	else
+	{
+		printStatus();
+		std::cout << " fell down and died." << std::endl;
+	}
+};
+
+
+
+
+
+bool PlayerRPS::act()
+{
+	enum rpsType { Rock, Paper, Scissors };
+	auto rps = [] {
+		float p = possibility(random_engine);
+
+		if (p < float(1.f / 3.f))
+			return rpsType::Rock;
+		else if (p < float(2.f / 3.f))
+			return rpsType::Paper;
+		else
+			return  rpsType::Scissors;
+	};
+
+	rpsType myRPS, yourRPS;
+	do {
+		myRPS = rps();
+		yourRPS = rps();
+	}
+	while(myRPS == yourRPS);
+	
+	if (myRPS == Rock)
+	{
+		if (yourRPS == Paper)
+			return false;
+		else
+			return true;
+	}
+	else if (myRPS == Paper)
+	{
+		if (yourRPS == Scissors)
+			return false;
+		else
+			return true;
+	}
+	else
+	{
+		if (yourRPS == Rock)
+			return false;
+		else
+			return true;
+	}
+}
+
+
+void PlayerRPS::dyingMessage()
+{
+	printStatus();
+	std::cout << " died." << std::endl;
+};
